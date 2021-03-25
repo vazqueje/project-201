228b560dbff7ba12c9030d1664218e771b777046
Ash15157 <linr5@miamioh.edu>
2021-03-25 17:30:41 -0400

    Create Entry.java
    The Game Entry Class

---
+++ Entry.java
@@ -0,0 +1,77 @@
+import java.sql.*;
+import java.util.ArrayList;
+public class Entry {
+private String name;
+private String description;
+private String genre;
+private String developer;
+private Date publishDate;
+private String esrbRating;
+private ArrayList<String> commentList;
+
+public Entry(String name, String description, String genre, String developer, Date publishDate, String esrbRating, ArrayList<String> commentList) {
+	this.name = name;
+	this.description = description;
+	this.genre = genre;
+	this.developer = developer;
+	this.publishDate = publishDate;
+	this.esrbRating = esrbRating;
+	this.commentList = commentList;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public String getGenre() {
+	return genre;
+}
+
+public void setGenre(String genre) {
+	this.genre = genre;
+}
+
+public String getDeveloper() {
+	return developer;
+}
+
+public void setDeveloper(String developer) {
+	this.developer = developer;
+}
+
+public Date getPublishDate() {
+	return publishDate;
+}
+
+public void setPublishDate(Date publishDate) {
+	this.publishDate = publishDate;
+}
+
+public String getEsrbRating() {
+	return esrbRating;
+}
+
+public void setEsrbRating(String esrbRating) {
+	this.esrbRating = esrbRating;
+}
+
+public ArrayList<String> getCommentList() {
+	return commentList;
+}
+
+public void setCommentList(ArrayList<String> commentList) {
+	this.commentList = commentList;
+}
+}
