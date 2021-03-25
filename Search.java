cc55c9b96d92e6b44525edd3dca617ba1db9bd83
vazqueje <64759363+vazqueje@users.noreply.github.com>
2021-03-25 17:11:11 -0400

    Update Search.java

--- Search.java
+++ Search.java
@@ -4,41 +4,36 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 //SQLConnection.java is necessary to execute a search
-//Usage example:
-//Search newSearch = new Search("Donkey Kong 64");
-//newSearch.fetchSearch(newSearch.userSearch);
-//^^will cleanup to eliminate redundancy
 public class Search {
-	String userSearch; //The string that the user will enter to find a game, only works for the exact name of the game right now
-	Connection con = SQLConnection.getConnection(); // The connection to the game_catalog database
-	
+	String userSearch;
+	Connection con = SQLConnection.getConnection();
 	
 	public Search(String userSearch) {
 		this.userSearch = userSearch;
 	}
 	
-	/**
-	* Uses the user's search demands to query the database and returns all results as a string.
-	**/
 	public String fetchSearch(String userSearch) {
 		Statement stmt = null;
 		ResultSet rs = null;
 		String result = "";
 		try {
 			 stmt = con.createStatement();
-			 //create SQL query
-			 String query = "SELECT * FROM game_catalog WHERE name=\""+userSearch+"\";";
-			 //execute the SQL query
+			 String query = "SELECT * FROM game_catalog WHERE name LIKE \"%"+userSearch+"%\";";
 			 rs = stmt.executeQuery(query);
-			 //iterates through each line of the result table
-			 while(rs.next()){
-				   String name = rs.getString("name");
-				   result += name;
-				   if(rs.isBeforeFirst()) {
-					   result += "\n";
-				   }
-				   
-				}
+			 
+			 if (rs.next() == false) {
+			        System.out.println("Your query did not match any results.");
+			      } else {
+
+			        do {
+			          String name = rs.getString("name");
+			          result += name+"\n";
+			        } while (rs.next());
+			      }
+			 
+			 
+			 //
+			 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
