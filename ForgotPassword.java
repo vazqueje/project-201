83217a7b9b95efa75a129852ba0d6d37b6388406
Ash15157 <linr5@miamioh.edu>
2021-04-25 16:41:00 -0400

    Added forgotPassword and updated UserVerify
    ForgotPassword is a new class that returns a User's password given a username. I also updated UserVerify so that it has a method to create new accounts.

---
+++ ForgotPassword.java
@@ -0,0 +1,56 @@
+import java.sql.*;
+
+/**
+ * @author Raymond Lin
+ *
+ */
+public class ForgotPassword {
+	private String username;
+	Connection conn = SQLConnection.getConnection();
+
+	
+	/**
+	 * @param username takes in the username of the user.
+	 */
+	public ForgotPassword(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return The username that was inputted into the constructor
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username sets the username for forgotten password
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public User returnUser() {
+		try {
+			//if someone tries to find the guest account's information it will just return null
+			if(this.getUsername().equals("Guest")) {
+				return null;
+			}
+			PreparedStatement p1 = conn.prepareStatement("Select * from user_table WHERE username=?", Statement.RETURN_GENERATED_KEYS);
+			p1.setString(1, this.getUsername());
+			p1.execute();
+			ResultSet generatedKeys = p1.getResultSet();
+			if (!generatedKeys.next()) {
+				return null;
+			}
+			generatedKeys.beforeFirst();
+			generatedKeys.first();
+			return new User(generatedKeys.getString(1),generatedKeys.getString(2),generatedKeys.getString(3),generatedKeys.getDate(4),generatedKeys.getInt(5), false);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+	}
+	
+
+}
--- UserVerify.java
+++ UserVerify.java
@@ -1,30 +1,71 @@
 import java.sql.*;
+/**
+ * @author Raymond Lin
+ *
+ */
 public class UserVerify {
 	private String username;
 	private String password;
+	private String email;
+	private Date dob;
 	Connection conn = SQLConnection.getConnection();
 	
+	/**
+	 * Use this constructor for getUser method
+	 * @param username the username of the person trying to login
+	 * @param password the password of the person trying to login
+	 */
 	public UserVerify(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
+	
+	/**
+	 * Use this constructor for createAccount method
+	 * @param username the username of the person trying to create an account
+	 * @param password the password of the person trying to create an account
+	 * @param email the email of the person trying to create an account
+	 * @param dob the date of birth of the person trying to create an account
+	 */
+	public UserVerify(String username, String password, String email, Date dob ) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.dob = dob;
+	}
 
+	/**
+	 * @return the username
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * @param username the username
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * @return gets the password
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * @param password the password
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	/**
+	 * @return if the user is found it will return the user's account if not it will return a null.
+	 * @throws SQLException 
+	 */
 	public User getUser() throws SQLException {
 		PreparedStatement p1 = conn.prepareStatement("Select * From user_table WHERE username=? AND password=?",Statement.RETURN_GENERATED_KEYS);
 		p1.setString(1, this.getUsername());
@@ -35,22 +76,30 @@ public class UserVerify {
 			return null;
 		}
 		generatedKeys.beforeFirst();
-//		if (rs.next() == false) {
-//			System.out.println("ResultSet in empty in Java");
-//		} else { 
-//			do {
-//				String data = rs.getString();
-//				System.out.println(data);
-//			} while (rs.next)
-//		}
 		
-//		if(generatedKeys.getFetchSize() == 0) {
-//			return null;
-//		}
 		generatedKeys.first();
 		
 		return new User(generatedKeys.getString(1),generatedKeys.getString(2),generatedKeys.getString(3),generatedKeys.getDate(4),generatedKeys.getInt(5));
 		
 	}
+	
+	/**
+	 * @return true if the account was added into the database and false if the account was unable to be added.
+	 */
+	public boolean createAccount() {
+		try {
+			PreparedStatement p2 = conn.prepareStatement("Insert INTO user_table(username, password, email, dob, privileges) VALUES (?,?,?,?,0)", Statement.RETURN_GENERATED_KEYS);
+			p2.setString(1, this.username);
+			p2.setString(2, this.password);
+			p2.setString(3, this.password);
+			p2.setDate(4, this.dob);
+			p2.execute();
+			return true;
+		} catch (SQLException e) {
+			return false;
+		}
+	}
+	
+	
 	
 }
