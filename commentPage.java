3239761e936028b5472b50f13cd119d3e2680c30
vazqueje <64759363+vazqueje@users.noreply.github.com>
2021-05-01 16:22:04 -0400

    Update commentPage.java

--- commentPage.java
+++ commentPage.java
@@ -39,7 +39,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.sql.*;
 import javax.swing.JTextArea;
-public class CommentPage extends JFrame {
+public class CommentPage extends JFrame implements ActionListener{
 	private JPanel contentPane;
 	int xx,xy;
 	private int searchCount;
@@ -49,7 +49,14 @@ public class CommentPage extends JFrame {
 	Entry entry;
 	CommentSQL commentSQL;
 	CommentSection comments;
-
+	private JButton home;
+	private JButton profile;
+	private JButton favorites;
+	private JButton submit;
+	private JButton addFavorite;
+	private JTextArea commentArea;
+	private JTextField commentTitle;
+	
 	/**
 	 * Creates a new main page frame where the user can view all games in the library 
 	 * @param user the type of user viewing the page
@@ -99,11 +106,6 @@ public class CommentPage extends JFrame {
 				     panel.setBounds(0, 59, 1578, 140);
 				     contentPane.add(panel);
 				     
-				     JLabel gameCover = new JLabel("");
-				     gameCover.setBounds(1293, -18, 220, 284);
-				     panel.add(gameCover);
-				     gameCover.setIcon(icon);
-				     
 				     JLabel gameName = new JLabel("Gaming Library");
 				     gameName.setBounds(41, 13, 1525, 123);
 				     
@@ -121,12 +123,19 @@ public class CommentPage extends JFrame {
 							// Handle exception
 						}
 				     panel.add(gameName);
+				     
+				     JLabel gameCover = new JLabel("");
+				     gameCover.setBounds(1342, 26, 196, 247);
+				     panel.add(gameCover);
+				     gameCover.setIcon(icon);
 		//Set up navigation bar
 		JPanel navpanel = new JPanel();
 		navpanel.setBackground(new Color(25,24,26));
 		navpanel.setBounds(0, 0, 1578, 63);
 		navpanel.setLayout(null);
 		contentPane.add(navpanel);
+		
+		
 		
 		//add close window button
 		JLabel lbl_close = new JLabel("X");
@@ -147,20 +156,36 @@ public class CommentPage extends JFrame {
 		Border emptyBorder = BorderFactory.createEmptyBorder();
 		
 		//Add profile button to navbar
-		JButton profile = new JButton("Profile");
-		profile.setFocusPainted(false);
-		profile.setForeground(Color.WHITE);
-		profile.setBorder(emptyBorder);
-		profile.setBounds(262, 0, 164, 63);
-		profile.setBackground(new Color(25,24,26));
+		home = new JButton("Home");
+		home.setForeground(Color.WHITE);
+		home.setFont(null);
+		home.setFocusPainted(false);
+		home.setBackground(new Color(25, 24, 26));
+		home.setBounds(171, 0, 164, 63);
+		navpanel.add(home);
+		home.setFocusPainted(false);
+		home.setForeground(Color.WHITE);
+		home.setBorder(emptyBorder);
+		home.setBounds(80, 0, 164, 63);
+		home.setBackground(new Color(25,24,26));
 		
 		//Add favorites button to navbar
-		JButton favorites = new JButton("Favorites");
-		favorites.setBounds(70, 0, 164, 63);
+		favorites = new JButton("Favorites");
+		favorites.setBounds(480, 0, 164, 63);
 		favorites.setForeground(Color.WHITE);
 		favorites.setFocusPainted(false);
 		favorites.setBorder(emptyBorder);
 		favorites.setBackground(new Color(25,24,26));
+		
+		profile = new JButton("Profile");
+		profile.setForeground(Color.WHITE);
+		profile.setFont(null);
+		profile.setFocusPainted(false);
+		profile.setBackground(new Color(25, 24, 26));
+		profile.setBorder(emptyBorder);
+		profile.setBounds(280, 0, 164, 63);
+		navpanel.add(profile);
+		
 		//register main font
 		try {
 		     //Returned font is of pt size 1
@@ -172,12 +197,14 @@ public class CommentPage extends JFrame {
 
 		     profile.setFont(font2);
 		     favorites.setFont(font2);
+		     home.setFont(font2);
 
 		} catch (IOException|FontFormatException e) {
 		     // Handle exception
 		}
 		navpanel.add(profile);
 		navpanel.add(favorites);
+		
 		   
 		//Derive and return a 12 pt version:
 		//Need to use float otherwise
@@ -191,6 +218,10 @@ public class CommentPage extends JFrame {
 
 		navpanel.add(smallIcon);
 		
+		
+		
+		
+		
 		//create panel to display catalog entries
 		JPanel tablepanel = new JPanel();
 		
@@ -199,7 +230,7 @@ public class CommentPage extends JFrame {
 		
 		tablepanel.setBackground(Color.WHITE);
 		tablepanel.setLayout(null);
-		tablepanel.setBounds(0, 325, 1566, 730);
+		tablepanel.setBounds(0, 316, 1566, 730);
 		contentPane.add(tablepanel);
 		
 
@@ -208,14 +239,69 @@ public class CommentPage extends JFrame {
 	                new TableDisplay().setVisible(true);
 	            }
 	        });
-	        
-	    commentSQL = new CommentSQL(entry);
-	    CommentSection comments = commentSQL.getCommentSection();  
-	    CommentDisplay cd = new CommentDisplay(comments);
 	    
-		cd.setBorder(new LineBorder(new Color(255, 255, 255), 10));
-		cd.setBounds(12,13,1542,492);
-		tablepanel.add(cd);
+	    commentSQL = new CommentSQL(entry);
+	    if(commentSQL == null) {
+	    	JLabel noComment = new JLabel("There are no comments. Be the first one to comment on this game!");
+	    	noComment.setFont(new Font("Tahoma", Font.PLAIN, 22));
+	    	noComment.setForeground(Color.LIGHT_GRAY);
+	    	noComment.setBounds(40, 13, 294, 27);
+	    	tablepanel.add(noComment);
+	    }else {
+	    	 CommentSection comments = commentSQL.getCommentSection();  
+	 	    CommentDisplay cd = new CommentDisplay(comments);
+	    	cd.setBorder(new LineBorder(new Color(255, 255, 255), 10));
+			cd.setBounds(12,13,1542,492);
+			tablepanel.add(cd);
+	    }
+	   
+	    
+		
+		
+		JLabel lblShareYourThoughts = new JLabel("Add a comment:");
+		lblShareYourThoughts.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblShareYourThoughts.setBounds(40, 500, 294, 27);
+		tablepanel.add(lblShareYourThoughts);
+		
+		if(user.getPrivileges() != -1) {
+			commentTitle = new JTextField();
+			commentTitle.setBorder(new LineBorder(new Color(51, 51, 51), 3));
+			commentTitle.setBounds(38, 550, 695, 35);
+			commentTitle.setColumns(10);
+			tablepanel.add(commentTitle);
+			
+			commentArea = new JTextArea();
+			commentArea.setBorder(new LineBorder(new Color(51, 51, 51), 3));
+			commentArea.setBounds(40, 610, 693, 96);
+			tablepanel.add(commentArea);
+			
+			submit = new JButton("Submit");
+			submit.setForeground(Color.WHITE);
+			submit.setFont(new Font("Dialog", Font.PLAIN, 20));
+			submit.setFocusPainted(false);
+			submit.setBackground(new Color(25, 24, 26));
+			submit.setBounds(775, 641, 164, 63);
+			tablepanel.add(submit);
+			
+			
+			
+			JLabel lblTitle = new JLabel("Title:");
+			lblTitle.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			lblTitle.setBounds(40, 524, 294, 27);
+			tablepanel.add(lblTitle);
+			
+			JLabel lblComment = new JLabel("Comment:");
+			lblComment.setFont(new Font("Tahoma", Font.PLAIN, 15));
+			lblComment.setBounds(40, 582, 294, 27);
+			tablepanel.add(lblComment);
+		}else {
+			JLabel guestComment = new JLabel("You must be logged into to comment.");
+			guestComment.setFont(new Font("Tahoma", Font.PLAIN, 22));
+			guestComment.setBounds(40, 524, 294, 27);
+			guestComment.setForeground(Color.LIGHT_GRAY);
+			tablepanel.add(guestComment);
+		}
+		
 		 gameDesc = new JTextArea();
 		try {
 		     //Returned font is of pt size 1
@@ -229,32 +315,83 @@ public class CommentPage extends JFrame {
 			e.printStackTrace();
 		}
 		    
-		     gameDesc.setBounds(36, 212, 1503, 91);
+		     gameDesc.setBounds(36, 212, 1302, 91);
 		     gameDesc.setEditable(false);
 		     gameDesc.setText(entry.getDescription());
 		     contentPane.add(gameDesc);
+		     
+		     addFavorite = new JButton("Add to favorites");
+		     addFavorite.setForeground(Color.WHITE);
+		     addFavorite.setFont(new Font("Dialog", Font.PLAIN, 21));
+		     addFavorite.setFocusPainted(false);
+		     addFavorite.setBackground(new Color(25, 24, 26));
+		     addFavorite.setBounds(1351, 212, 196, 91);
+		     contentPane.add(addFavorite);
 		     		     //titleLabel3.setFont(font);
 		     		     
 
 		
 		
-		/**
-		 * Event handler for favorites button. Disposes the current page and opens up the favorites page.
-		 */
-		favorites.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
+		
 				
-				dispose();
-				System.out.println(mainUser.getUsername());
-				if(mainUser == null) {
-					System.out.println("Null");
-				}
-				new FavoritesPageGUI(mainUser);
-			}
-		});
-		
+	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
+
+		if (e.getSource() == home) {
+			dispose();
+			new MainPage(mainUser);
+		}
+		else if (e.getSource() == addFavorite) {
+			//NOT WORKING //OLDCODE
+			FavoriteListSQL list = new FavoriteListSQL(entry, mainUser);
+			Boolean bool;
+			try {
+				bool = list.addFavorite();
+				if (!bool) JOptionPane.showMessageDialog(null, "Game already in Favorites", "Error", JOptionPane.WARNING_MESSAGE);
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+		else if (e.getSource() == submit) {
+			if (commentTitle.getText().length()==0 || commentArea.getText().length()==0) {
+				return;
+			}
+			System.out.println("clicked");
+			String title = commentTitle.getText();
+			String description =  commentArea.getText();
+			try {
+				commentSQL.addComment(title, description, entry.getName());
+			} catch (SQLException e1) {
+				// TODO Auto-generated catch block
+				System.out.println("Failed");
+				e1.printStackTrace();
+			}
+			dispose();
+			new CommentPage(mainUser, entry);
+			//frame.setVisible(true);
+		}
+		else {
+			System.out.println("TO-DO");
+			//REMOVE COMMENT - NOT WORKING
+//			for (int i = 0; i < list.size(); i++) {
+//				if (e.getSource() == list.get(i)) {
+//					String str = list.get(i).getText().substring(8);
+//					int id = Integer.parseInt(str);
+//					try {
+//						commentSQL.removeComment(id);
+//					} catch (SQLException e1) {
+//						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+//					}
+//				}
+//			}
+//			
+//			frame.dispose();
+//			new commentPage(mainUser, entry);
+		}
 	}
 }
